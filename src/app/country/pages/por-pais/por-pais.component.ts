b61b7bc6bcf838f3bddb126397746fb6403d6bf0
eideard-hm <ehhernandez81@misena.edu.co>
2021-12-27 10:09:49 -0500

    App terminada usando angular, rutas, rxjs

--- src/app/country/pages/por-pais/por-pais.component.ts
+++ src/app/country/pages/por-pais/por-pais.component.ts
@@ -6,31 +6,48 @@ import { CountryService } from '../../services/country.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
-  ]
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class PorPaisComponent {
-
   termino: string = '';
   hayError: boolean = false;
   results: Country[] = [];
 
-  constructor(private countryService: CountryService) { }
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
+
+  constructor(private countryService: CountryService) {}
 
   buscar(termino: string) {
     this.termino = termino;
     this.hayError = false;
-    this.countryService.searchByCountry(this.termino)
-      .subscribe({
-        next: (res) => this.results = res,
-        error: () => {
-          this.hayError = true;
-          this.results = [];
-        }
-      })
+    this.mostrarSugerencias = false;
+    this.countryService.searchByCountry(this.termino).subscribe({
+      next: (res) => (this.results = res),
+      error: () => {
+        this.hayError = true;
+        this.results = [];
+      },
+    });
   }
 
   sugerencia(termino: string) {
     this.hayError = false;
-    console.log(termino)
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.countryService.searchByCountry(termino).subscribe({
+      next: (countries) => (this.paisesSugeridos = countries.slice(0, 5)),
+      error: () => (this.paisesSugeridos = []),
+    });
+  }
+
+  buscarSugerido(termino:string){
+     this.buscar(termino);
   }
 }
