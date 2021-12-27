8893483a7835750503967167f5891c1f8601145c
eideard-hm <ehhernandez81@misena.edu.co>
2021-12-26 22:16:02 -0500

    Finish first part of this project with angular 13 and using rxjs library

--- src/app/country/pages/ver-pais/ver-pais.component.ts
+++ src/app/country/pages/ver-pais/ver-pais.component.ts
@@ -1,27 +1,29 @@
+import { Country } from './../../interfaces/country.interface';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+
 import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  country!: Country;
 
   constructor(
     private activitedRoute: ActivatedRoute,
     private countryService: CountryService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.activitedRoute.params
       .pipe(
-        switchMap(({ id }) => this.countryService.searchByCode(id))
+        switchMap(({ id }) => this.countryService.searchByCode(id)),
+        tap(console.log)
       )
-      .subscribe(country => console.log(country))
+      .subscribe((country) => (this.country = country[0]));
   }
-
 }
